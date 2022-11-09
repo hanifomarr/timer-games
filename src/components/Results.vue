e6f9040491f7cdfee14059b5750f7b750442ca4e
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2022-11-10 01:07:14 +0800

    Show rank based on timer

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,27 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <p>Rank : {{ rank }}</p>
 </template>
 
 <script>
 export default {
-props: ["score"],
-}
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Damn Fast";
+    } else if (this.score < 400) {
+      this.rank = "Good but not Fast";
+    } else {
+      this.rank = "slow";
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
